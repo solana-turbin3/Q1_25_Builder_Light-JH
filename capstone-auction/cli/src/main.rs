7d8f8ac6d1cb8b007baf7c2d11084a6e6b68a2a0
Light-JH <phoebehu@outlook.com>
2025-02-26 15:45:15 -0600

    edit init_house cli

--- capstone-auction/cli/src/main.rs
+++ capstone-auction/cli/src/main.rs
@@ -17,8 +17,13 @@ struct Cli {
     /// Path to keypair file used for signing
     #[arg(short, long, value_name = "PATH")]
     keypair_path: Option<PathBuf>,
+
     #[command(subcommand)]
     command: Command,
+
+    /// Name to identify the auction house.
+    #[arg(short, long, value_name = "NAME", default_value = "auction_house")]
+    auction_house_name: String,
 }
 
 #[derive(Subcommand)]
@@ -26,15 +31,21 @@ enum Command {
     /// Initialize the AuctionHouse.
     InitHouse {
         /// Fee in basis points taken for successful auctions.
+        #[arg(value_name = "BASIS_POINTS")]
         fee: u16,
-        /// Name to identify the auction house.
-        name: String,
     },
 
+    /// Initialize a new auction
     InitAuction {
-        starting_price: u64,
+        /// The starting price.
+        starting_price: String,
+        /// The slot the auction will end on.
         end_slot: u64,
-        amount: u64,
+        /// The number of tokens to auction off.
+        amount: String,
+
+        /// The number of decimals to be used for the price.
+        #[clap(long, short, default_value = "9")]
         decimal: u8,
     },
 
@@ -52,7 +63,14 @@ fn main() {
     let Cli {
         keypair_path,
         command,
+        auction_house_name,
     } = Cli::parse();
+
+    let (auction_house, _auction_house_bump) = Pubkey::find_program_address(
+        &[b"house", auction_house_name.as_bytes()],
+        &AuctionProgram::id(),
+    );
+
     let rpc_url = "http://127.0.0.1:8899";
     let client = RpcClient::new(rpc_url);
 
@@ -64,21 +82,29 @@ fn main() {
     let keypair = read_keypair_file(&keypair_path).expect("Couldn't find wallet file");
 
     match command {
-        Command::InitHouse { fee, name } => {
-            println!("Initializing house with fee: {} and name: {}", fee, name);
+        Command::InitHouse { fee } => {
+            println!(
+                "Initializing house with fee: {} and name: {}",
+                fee, auction_house_name
+            );
 
             let recent_blockhash = client
                 .get_latest_blockhash()
                 .expect("recent blockhash exists");
-            let (auction_house, _auction_house_bump) =
-                Pubkey::find_program_address(&[b"house", name.as_bytes()], &AuctionProgram::id());
+            let (auction_house, _auction_house_bump) = Pubkey::find_program_address(
+                &[b"house", auction_house_name.as_bytes()],
+                &AuctionProgram::id(),
+            );
             let transaction = AuctionProgram::init_house(
                 &[
                     &keypair.pubkey(),
                     &auction_house,
                     &solana_sdk::system_program::id(),
                 ],
-                &InitHouseArgs { fee, name },
+                &InitHouseArgs {
+                    fee,
+                    name: auction_house_name,
+                },
                 Some(&keypair.pubkey()),
                 &[&keypair],
                 recent_blockhash,
